4cb34020cf92073974ffd647ca089682713a0ab8
Isadora Cardoso <101332900+isadoraacardoso@users.noreply.github.com>
2025-11-03 12:59:50 -0300

    correção

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,7 +1,7 @@
-import { bootstrapApplication, type BootstrapContext } from '@angular/platform-browser';
+import { bootstrapApplication } from '@angular/platform-browser';
 import { App } from './app/app';
 import { config } from './app/app.config.server';
 
-const bootstrap = (context: BootstrapContext) => bootstrapApplication(App, config, context);
-
-export default bootstrap;
+export default function () {
+  return bootstrapApplication(App, config);
+}
